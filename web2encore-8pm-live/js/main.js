0bf26df3c2806c506e336e1a3e866690babb417c
user2 <user2@Mac2s-iMac.local>
2019-05-16 14:03:13 -0700

    Updated all pages with iframe and added script to get url parameters into iframe.

--- web2encore-8pm-live/js/main.js
+++ web2encore-8pm-live/js/main.js
@@ -10,3 +10,18 @@ $(document).ready(function() {
     $('h3, p').toggle('slow')
   })
 })
+
+function getQueryVariable(variable)
+{
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if(pair[0] == variable){return pair[1];}
+  }
+  return(false);
+}
+
+var affId = getQueryVariable('hop')
+var newLink = `http://${affId}.adtrics.hop.clickbank.net/?pid=8`;
+$('#affTrack').attr('src', newLink);
