23c34ba255833e9cb4789da065c6266580d1b2c9
Warren-Ipro <warren@iprogroup.ca>
2019-05-23 14:45:05 -0700

    Created Page.

---
+++ aa-replay/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(() => {
+  $('.web-select').on('click', (e) => {
+    id = e.target.id
+    if(id === "web-select-1") {
+      toggleOutVimeo("#video2", "#web2");
+      toggleInVimeo("#video1", "#web1");
+    } else {
+      toggleOutVimeo("#video1", "#web1");
+      toggleInVimeo("#video2", "#web2");
+    }
+  })
+})
+
+const toggleOutVimeo = (id, webinarID) => {
+  var iframe = $(id);
+  var player = new Vimeo.Player(iframe);
+  player.pause();
+  $(webinarID).hide(200);
+}
+
+const toggleInVimeo = (id, webinarID) => {
+  var iframe = $(id);
+  var player = new Vimeo.Player(iframe);
+  $(webinarID).show(200);
+  player.play();
+}
