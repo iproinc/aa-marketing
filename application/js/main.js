a48be686dba854838b32ecc03fc798493983d8eb
reneeipro <renee@iprogroup.ca>
2021-08-11 13:36:36 -0700

    added sidebar

--- application/js/main.js
+++ application/js/main.js
@@ -11,3 +11,19 @@ function playVideo() {
   $('.play-overlay').removeClass("z-10");
   autoPlay.play();
 }
+
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+  
+    if (scroll >= 900) {
+        $(".sidebar").addClass("fixed top-10 right-0 lg:mr-[5%] xl:mr-[11%] 2xl:mr-[18%] 3xl:mr-[22%] 4xl:mr-[28%] w-[300px] hidden lg:block");
+        $(".left").addClass("lg:w-2/3");
+        $(".sidebar-title").removeClass("hidden");
+        $(".sidebar-button").removeClass("hidden");
+    } else {
+        $(".sidebar").removeClass("fixed top-10 right-0 lg:mr-[5%] xl:mr-[11%] 2xl:mr-[18%] 3xl:mr-[22%] 4xl:mr-[28%] w-[300px] hidden lg:block");
+        $(".left").removeClass("lg:w-2/3");
+        $(".sidebar-title").addClass("hidden");
+        $(".sidebar-button").addClass("hidden");
+    }
+});
