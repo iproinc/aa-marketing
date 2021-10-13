5c47779e0e55b860238ffd63ee20a597b015a36e
Warren-Ipro <warren@iprogroup.ca>
2021-10-12 17:20:14 -0700

    Project set up

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'san-marino': {
+          '50': '#f6f8fa', 
+          '100': '#edf0f6', 
+          '200': '#d3dae8', 
+          '300': '#b8c3da', 
+          '400': '#8396be', 
+          '500': '#4E69A2', 
+          '600': '#465f92', 
+          '700': '#3b4f7a', 
+          '800': '#2f3f61', 
+          '900': '#26334f'
+        },
+        'mariner': {
+          '50': '#f6f8fd', 
+          '100': '#edf1fb', 
+          '200': '#d2ddf5', 
+          '300': '#b6c8ef', 
+          '400': '#809fe2', 
+          '500': '#4976D6', 
+          '600': '#426ac1', 
+          '700': '#3759a1', 
+          '800': '#2c4780', 
+          '900': '#243a69'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
